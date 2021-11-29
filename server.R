edf033c6fe2d8101556ffe3ad93e964fd719899d
Pawan Rama Mali <prm@outlook.in>
2021-11-30 02:21:53 +0530

    Update server.R

--- server.R
+++ server.R
@@ -3,8 +3,8 @@ server <- function(input, output, session) {
   #* Login Functionality ----
   source('./server/login.R', local = TRUE)
 
-  # * Hashing Functionality ----
-  source('./server/hash.R', local = TRUE)
+  # * Coding Functionality ----
+  source('./server/code_runner.R', local = TRUE)
   
   
 }
