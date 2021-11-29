bb5af0be36ffc0152f27b724364ecba8fe57d4d6
Pawan Rama Mali <prm@outlook.in>
2021-11-29 19:37:16 +0530

    Update build_page.R

--- ui/admin/build_page.R
+++ ui/admin/build_page.R
@@ -3,51 +3,63 @@
 tabPanel("Build",
          value = "Build",
          fluidPage
-         (fluidRow(
-           h3("Build you Hashed and Salted Password"),
-           
-           column(
-             2,
-             selectInput(
-               inputId = "select_hash",
-               label = "Select Hash Function",
-               choices = c(
-                 "md5",
-                 "sha1",
-                 "crc32",
-                 "sha256",
-                 "sha512",
-                 "xxhash32",
-                 "xxhash64",
-                 "murmur32",
-                 "spookyhash",
-                 "blake3"
+         (
+           fluidRow(
+             h3("Build you Hashed and Salted Password"),
+             
+             column(
+               3,
+               selectInput(
+                 inputId = "select_hash",
+                 label = "Select Hash Function",
+                 choices = c(
+                   "md5",
+                   "sha1",
+                   "crc32",
+                   "sha256",
+                   "sha512",
+                   "xxhash32",
+                   "xxhash64",
+                   "murmur32",
+                   "spookyhash",
+                   "blake3"
+                 ),
+                 selected = "sha512",
+                 multiple = FALSE
                ),
-               selected = "sha512",
-               multiple = FALSE
-             ),
-           ),
-           
-           column(
-             2,
-             textInput(
-               inputId = "text_to_hash",
-               value = "",
-               label = "Enter Password to hash : "
              ),
              
-             actionButton(
-               inputId = "start_hash",
-               label = "Submit",
-               icon = NULL,
-               width = "100%",
-               class = "btn-success"
+             column(
+               4,
+               textAreaInput(
+                 inputId = "text_to_hash",
+                 value = "",
+                 label = "Enter Password to hash : ",
+                 width = "100%",
+                 height = "400px",
+               ),
+               br(),
+               
+               actionButton(
+                 inputId = "start_hash",
+                 label = "Submit",
+                 icon = NULL,
+                 width = "100%",
+                 class = "btn-success"
+               )
              )
            ),
-           column(3,
-                  fluidRow(
-                    verbatimTextOutput(outputId = "hashed_output",
-                                       placeholder = FALSE)
-                  ))
            
-         )))
+           fluidRow(
+             br(),
+             column(11,
+             textInput(inputId = "hashed_output",
+                       label = "",
+                       
+                                placeholder = FALSE)
+             ),
+             column(1,
+             uiOutput("clip"))
+           )
+           
+         ))
